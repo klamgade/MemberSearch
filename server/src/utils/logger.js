b4461d32b1f8604dfd6dc18a5d3cd2309d621e14
prakash-pra <prakash287902@gmail.com>
2021-11-25 20:29:16 +1300

    changes

--- server/src/utils/logger.js
+++ server/src/utils/logger.js
@@ -2,14 +2,14 @@ const config = require('../config');
 const winston = require('winston');
 
 const logger = winston.createLogger({
-	level: config.get('logLevel') || 'info',
-	transports: [new winston.transports.Console()],
-	format: winston.format.combine(
-		winston.format.timestamp({
-			format: 'YYYY-MM-DD HH:mm:ss'
-		}),
-		winston.format.json()
-	)
+  level: config.get('logLevel') || 'info',
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+    winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    winston.format.json()
+  )
 });
 
 module.exports = logger;
